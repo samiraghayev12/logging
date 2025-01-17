29e19dd41a40be91e95525e683fd0094d17bd34c
Samir Aghayev <saaghayev@its.gov.az>
2025-01-17 11:55:34 +0400

    codes refactored

--- lib/storage/debug_tool.dart
+++ lib/storage/debug_tool.dart
@@ -1,79 +1,32 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:logging_service/presentation/page/debug_page.dart';
-import 'package:logging_service/service/shake_service.dart';
-import 'package:shake_flutter/shake_flutter.dart';
 
 class DebugTool {
-  ShakeService? shakeService;
-
   bool isOpened = false;
-
   start(
     BuildContext context,
     String apiKey,
   ) {
-    shakeService = ShakeService(
-      onPhoneShake: () async {
-        if (!isOpened) {
-          isOpened = true;
-          await showCupertinoDialog(
-            context: context,
-            barrierDismissible: true,
-            builder: (context) => CupertinoAlertDialog(
-              title: const Text(
-                "Xoş gəlmişsiniz!",
-                style: TextStyle(
-                  color: CupertinoColors.black,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18,
-                ),
+    OverlayEntry overlayEntry = OverlayEntry(
+      builder: (context) => GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onLongPress: () async {
+          if (!isOpened) {
+            isOpened = true;
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => const DebugPage(),
               ),
-              actions: [
-                CupertinoDialogAction(
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => const DebugPage(),
-                    ),
-                  ),
-                  isDefaultAction: true,
-                  child: const Text(
-                    "Log",
-                    style: TextStyle(
-                      color: CupertinoColors.activeBlue,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                CupertinoDialogAction(
-                  onPressed: () async {
-                    Navigator.of(context).pop();
-                    await Future.delayed(const Duration(milliseconds: 500));
-                    // await Shake.start(apiKey);
-                    await Shake.show();
-                  },
-                  isDefaultAction: true,
-                  child: const Text(
-                    "Feedback",
-                    style: TextStyle(
-                      color: CupertinoColors.activeBlue,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-          isOpened = false;
-        }
-      },
+            ).then((_) {
+              isOpened = false;
+            });
+          }
+        },
+        child: const SizedBox.expand(),
+      ),
     );
 
-    shakeService?.start();
-  }
-
-  stop() {
-    shakeService?.stop();
+    Overlay.of(context).insert(overlayEntry);
   }
 }
