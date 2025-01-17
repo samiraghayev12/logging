917a79fd3eeb95ae2b0045b854d88aad60b414d9
Samir Aghayev <saaghayev@its.gov.az>
2025-01-17 12:02:12 +0400

    Codes refactored

--- lib/storage/debug_tool.dart
+++ lib/storage/debug_tool.dart
@@ -3,30 +3,39 @@ import 'package:logging_service/presentation/page/debug_page.dart';
 
 class DebugTool {
   bool isOpened = false;
-  start(
+
+  void start(
     BuildContext context,
     String apiKey,
   ) {
-    OverlayEntry overlayEntry = OverlayEntry(
-      builder: (context) => GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onLongPress: () async {
-          if (!isOpened) {
-            isOpened = true;
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => const DebugPage(),
-              ),
-            ).then((_) {
-              isOpened = false;
-            });
-          }
-        },
-        child: const SizedBox.expand(),
-      ),
-    );
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final overlayContext = Navigator.of(context).overlay?.context;
 
-    Overlay.of(context).insert(overlayEntry);
+      if (overlayContext != null) {
+        OverlayEntry overlayEntry = OverlayEntry(
+          builder: (_) => GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onLongPress: () async {
+              if (!isOpened) {
+                isOpened = true;
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const DebugPage(),
+                  ),
+                ).then((_) {
+                  isOpened = false;
+                });
+              }
+            },
+            child: const SizedBox.expand(),
+          ),
+        );
+
+        Overlay.of(overlayContext).insert(overlayEntry);
+      } else {
+        debugPrint("No Overlay found. Make sure MaterialApp or CupertinoApp is used.");
+      }
+    });
   }
 }
