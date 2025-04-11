f9aa31e4a35701544c664f323448ef56cf38c069
Samir Aghayev <saaghayev@its.gov.az>
2025-04-11 10:11:22 +0400

    Codes refactored

--- lib/storage/debug_tool.dart
+++ lib/storage/debug_tool.dart
@@ -3,46 +3,30 @@ import 'package:logging_service/presentation/page/debug_page.dart';
 
 class DebugTool {
   bool isOpened = false;
-  OverlayEntry? _overlayEntry;
-
-  // Başlangıç metodu
-  void start(BuildContext context) {
-    if (_overlayEntry != null) return; // Eğer zaten başlatılmışsa tekrarlama
-
-    _overlayEntry = OverlayEntry(
+  start(
+    BuildContext context,
+    String apiKey,
+  ) {
+    OverlayEntry overlayEntry = OverlayEntry(
       builder: (context) => GestureDetector(
-        onLongPress: () {
-          _openDebugPage(context);
+        behavior: HitTestBehavior.translucent,
+        onLongPress: () async {
+          if (!isOpened) {
+            isOpened = true;
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => const DebugPage(),
+              ),
+            ).then((_) {
+              isOpened = false;
+            });
+          }
         },
         child: const SizedBox.expand(),
       ),
     );
 
-    _insertOverlay(context);
-  }
-
-  // Overlay'i eklemek için metot
-  void _insertOverlay(BuildContext context) {
-    final overlay = Overlay.of(context);
-    overlay.insert(_overlayEntry!);
-    isOpened = true;
-  }
-
-  // Debug sayfasını açma
-  void _openDebugPage(BuildContext context) {
-    if (isOpened) {
-      Navigator.of(context).push(
-        MaterialPageRoute(builder: (_) => const DebugPage()),
-      );
-    }
-  }
-
-  // Overlay'i kaldırma
-  void removeOverlay() {
-    if (_overlayEntry != null) {
-      _overlayEntry!.remove();
-      _overlayEntry = null;
-      isOpened = false;
-    }
+    Overlay.of(context).insert(overlayEntry);
   }
 }
