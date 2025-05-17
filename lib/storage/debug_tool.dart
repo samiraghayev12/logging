56440c7423341a7e5930bb0488391c06e4006503
Samir Aghayev <saaghayev@its.gov.az>
2025-05-17 19:32:01 +0400

    ts

--- lib/storage/debug_tool.dart
+++ lib/storage/debug_tool.dart
@@ -2,42 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:logging_service/presentation/page/debug_page.dart';
 
 class DebugTool {
-  static final DebugTool _instance = DebugTool._internal();
-  bool _isInjected = false;
-  bool _isOpened = false;
-
-  factory DebugTool() => _instance;
-  DebugTool._internal();
-
-  void start(BuildContext context, String apiKey) {
-    if (_isInjected) return; // ✅ artıq əlavə olunubsa, yenidən etmə
-    _isInjected = true;
-
-    final overlayState = Overlay.of(context);
-    if (overlayState == null) {
-      debugPrint("❗ Overlay is null — skipping DebugTool injection.");
-      return;
-    }
-
-    final overlayEntry = OverlayEntry(
+  bool isOpened = false;
+  start(
+    BuildContext context,
+    String apiKey,
+  ) {
+    OverlayEntry overlayEntry = OverlayEntry(
       builder: (context) => GestureDetector(
         behavior: HitTestBehavior.translucent,
         onLongPress: () async {
-          if (!_isOpened) {
-            _isOpened = true;
-            await Navigator.push(
+          if (!isOpened) {
+            isOpened = true;
+            Navigator.push(
               context,
               MaterialPageRoute(
                 builder: (_) => const DebugPage(),
               ),
-            );
-            _isOpened = false;
+            ).then((_) {
+              isOpened = false;
+            });
           }
         },
         child: const SizedBox.expand(),
       ),
     );
 
-    overlayState.insert(overlayEntry);
+    Overlay.of(context).insert(overlayEntry);
   }
 }
+
+
