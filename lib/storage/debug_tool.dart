3c992641cc1a825f24e826a33ac339c866e843e9
Samir Aghayev <saghayev@rth.gov.az>
2026-05-19 09:49:52 +0400

    refactored

--- lib/storage/debug_tool.dart
+++ lib/storage/debug_tool.dart
@@ -1,34 +1,95 @@
 import 'package:flutter/material.dart';
-import 'package:logging_service/service/shake_service.dart';
-
 import '../presentation/page/debug_page.dart';
 
 class DebugTool {
   bool isOpened = false;
-  ShakeService? _shakeService;
   OverlayEntry? _overlayEntry;
+  BuildContext? _context;
 
   void start(BuildContext context, String apiKey) {
-    _shakeService = ShakeService(
-      onPhoneShake: () {
-        if (!isOpened) {
-          isOpened = true;
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => const DebugPage(),
-            ),
-          ).then((_) {
-            isOpened = false;
-          });
-        }
-      },
+    _context = context;
+    _showFloatingButton();
+  }
+
+  void _showFloatingButton() {
+    if (_overlayEntry != null) return;
+
+    _overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        right: 20,
+        bottom: 100,
+        child: _DebugFloatingButton(
+          onPressed: () => _openDebugPage(context),
+        ),
+      ),
     );
-    _shakeService?.start();
+
+    Overlay.of(_context!).insert(_overlayEntry!);
+  }
+
+  void _openDebugPage(BuildContext context) {
+    if (!isOpened) {
+      isOpened = true;
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => const DebugPage(),
+        ),
+      ).then((_) {
+        isOpened = false;
+      });
+    }
   }
 
   void dispose() {
-    _shakeService?.stop();
     _overlayEntry?.remove();
+    _overlayEntry = null;
   }
 }
+
+class _DebugFloatingButton extends StatefulWidget {
+  final VoidCallback onPressed;
+
+  const _DebugFloatingButton({required this.onPressed});
+
+  @override
+  State<_DebugFloatingButton> createState() => _DebugFloatingButtonState();
+}
+
+class _DebugFloatingButtonState extends State<_DebugFloatingButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeOut),
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: _scaleAnimation,
+      child: FloatingActionButton(
+        onPressed: widget.onPressed,
+        tooltip: 'Network Debug',
+        backgroundColor: Colors.indigo,
+        child: const Icon(Icons.bug_report_rounded),
+      ),
+    );
+  }
+}
+
